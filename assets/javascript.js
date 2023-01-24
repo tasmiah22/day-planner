6a8ad2c4dbdd0e71850e28ba2090048f846397c9
Tasmiah Begum <tasmiahbegum123@gmail.com>
2023-01-24 21:25:42 +0000

    edit js

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,32 +1,45 @@
 var container = $('.container');
-var time = moment().format("dddd, MMM Do YYYY", "");
-var today = moment().format("HH:mm:ss");
-var schedule = JSON.parse(localStorage.getItem('schedule')) || {
-    9:'',
-    10:'',
-    11:'',
-    12:'',
-    1:'',
-    2:'',
-    3:'',
-    4:'',
-    5:'',
-};
+var time = $('#currentTime');
+var today = $('#currentDay');
 
 // show todays date and time at the top of page //
 
-$("#currentday").html(today + time); 
+today.text(moment().format('dddd MMMM Do'));
+time.text(moment().format(HH,mm,ss));
 
-$(document).ready(function () {
-$(".saveBtn").on("click", function (){
-    var enterText = $(this).siblings(".description").val();
-    var entryTime = $(this).parent().attr("id");
-
-    localStorage.setItem(entryTime, enterText);
-    console.log("Saved.");
-
-});
 
 function timeTracker(){
     var timeNow = moment().hour();
 
+// colour code for past present & future events //
+
+$(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (timeBlock < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (timeBlock === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+    // get items from local storage //
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+  
